b7b4fae5dab5a53b2a3585451d8f97df1aab86e4
StefenClausen <stefanclaussen@me.com>
2014-05-13 12:09:47 +0100

    Complete ending lecture 194.  All works correctly.  Date for task is day, month year

--- Overdue/STCViewController.m
+++ Overdue/STCViewController.m
@@ -117,7 +117,7 @@
     cell.textLabel.text = task.title;
     
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"yyyy-MM-dd"];
+    [formatter setDateFormat:@"dd-MM-yyyy"];
     NSString *stringFromDate = [formatter stringFromDate:task.date];
     cell.detailTextLabel.text = stringFromDate;
     
